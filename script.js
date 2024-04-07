85ba12789d9567247849fba5d4d7b105583dfcbf
ashishstomar <23213257+ashishstomar@users.noreply.github.com>
2024-04-07 20:14:40 +0530

    Feature: All-Clear & Delete button

--- script.js
+++ script.js
@@ -1,31 +1,37 @@
 const displayTxt = document.querySelector('.displayTxt')
+displayTxt.textContent = '0'
 
 function add(x,y) {
     displayTxt.textContent = (x + y);
+    console.log(x+y)
 }
 
 function subtract(x,y) {
     displayTxt.textContent = (x - y);
+    console.log(x-y)
 }
 
 function multiply(x,y) {
     displayTxt.textContent = (x * y);
+    console.log(x*y)
 }
 
 function divide(x,y){
     displayTxt.textContent = (x / y);
+    console.log(x/y)
 }
 
 function factorial(num){
+    if (num === 0) return 1;
     fact = 1;
-    for(let i = num; i <= 1; i--) {
+    for(let i = num; i >= 1; i--) {
       fact = fact * i;
     }
     displayTxt.textContent = fact;
+    console.log(fact)
 }    
 
-const equal = document.querySelector("#equal");
-equal.addEventListener('click', () => {
+function operate(num1, operator, num2) {
     switch (operator) {
         case '+' :
             add(num1,num2);
@@ -40,23 +46,47 @@ equal.addEventListener('click', () => {
             divide(num1,num2);
             break;
         case '!':
-            factorial(num);
+            factorial(num1);
             break;            
+    }
+}
+
+
+let inputNum = [];
+let numButtons = document.querySelectorAll('.num');
+numButtons.forEach((numButton) => {
+    numButton.addEventListener('click', () => {
+        inputNum.push(numButton.textContent);
+        displayTxt.textContent = inputNum.join('');
+    })
+})
+
+
+let operatorButtons = document.querySelectorAll(".operator");
+operatorButtons.forEach((operatorButton) => {
+    operatorButton.addEventListener('click', () => {
+       let operatorButtonValue = operatorButton.textContent
+       if(operatorButtonValue === 'x'){
+       console.log(operatorButtonValue);
+        operate(inputNum.join(''),'x', 1)
+       }
+    })
+})
+
+let allClearBtn = document.querySelector("#allClear");
+allClearBtn.addEventListener('click', () => {
+    inputNum = [];
+    displayTxt.textContent = '0'
+})
+
+
+let deleteBtn = document.querySelector('#delete');
+deleteBtn.addEventListener('click', () => {
+    inputNum.pop()
+    displayTxt.textContent = inputNum.join('');
+    
+    if(inputNum.length === 0 ) {
+        displayTxt.textContent = '0'
     }
 })
 
-
-let num1 = 0;
-let num2 = 0;
-let operator = '';
-
-
-/*
-
-const btn = document.querySelectorAll(".btn")
-btn.forEach((x) => {
-    x.addEventListener('click', () => {
-        const displayTxt = document.querySelector('.displayTxt')
-        displayTxt.textContent = '7'
-    })
-})
