6acb45fbcb146da63580ee3a204fa929bc3c0fc5
ashishstomar <23213257+ashishstomar@users.noreply.github.com>
2024-04-10 16:54:53 +0530

    fix - Ensure calculator evaluates only a single pair of numbers at a time

--- script.js
+++ script.js
@@ -1,31 +1,27 @@
-let displayTxt = document.querySelector('.displayTxt')
+let previousOperand = document.querySelector('.previousOperand')
+let currentOperand = document.querySelector('.currentOperand')
 let allClearBtn = document.querySelector("#allClear");
 let deleteBtn = document.querySelector('#delete');
 let numButtons = document.querySelectorAll('.num');
 let operatorButtons = document.querySelectorAll(".operator");
 let decimalBtn = document.querySelector('#decimal');
+let posNegBtn = document.querySelector('#posNeg');
 let equalBtn = document.querySelector('#equal');
 
-displayTxt.textContent = '0'
-
 function add(x,y) {
-    displayTxt.textContent = (x + y);
-    console.log(x+y)
+    return x + y;
 }
 
 function subtract(x,y) {
-    displayTxt.textContent = (x - y);
-    console.log(x-y)
+    return x - y;
 }
 
 function multiply(x,y) {
-    displayTxt.textContent = (x * y);
-    console.log(x*y)
+    return x * y;
 }
 
 function divide(x,y){
-    displayTxt.textContent = (x / y);
-    console.log(x/y)
+    return x / y;
 }
 
 function factorial(num){
@@ -34,83 +30,122 @@ function factorial(num){
     for(let i = num; i >= 1; i--) {
       fact = fact * i;
     }
-    displayTxt.textContent = fact;
-    console.log(fact)
+    return fact;
 }    
 
 function operate(num1, operator, num2) {
 
     switch (operator) {
         case '+' :
-            add(num1,num2);
-            break;
+           return add(num1,num2);
         case '-' :
-            subtract(num1,num2);
-            break;
+           return subtract(num1,num2);
         case 'x' :
-            multiply(num1,num2);
-            break;
+           return multiply(num1,num2);
         case '/' :
-            divide(num1,num2);
-            break;
-        case 'X!':
-            factorial(num1);
-            break;            
+            return divide(num1,num2);
+        case '!':
+            return factorial(num1);           
     }
 }
 
 function allClear(){
     allClearBtn.addEventListener('click', () => {
-    inputNum = [];
-    displayTxt.textContent = '0'
-})
+    inputNum1 = '';
+    previousOperand.textContent = '';
+    currentOperand.textContent = '0';
+    })
 }
 
 
 function deleteNum() {
     deleteBtn.addEventListener('click', () => {
-        inputNum.pop()
-        displayTxt.textContent = inputNum.join('');
-    if(inputNum.length === 0 ) {
-        displayTxt.textContent = '0'
+        inputNum1 = inputNum1.slice(0, -1); 
+        currentOperand.textContent = inputNum1;
+    if(inputNum1.length === 0 ) {
+        currentOperand.textContent = '0'
     }
-})
+    })
+}
+
+previousOperand.textContent = '';
+currentOperand.textContent = '0';
+
+
+let inputNum1 = '';
+let inputNum2 = '';
+let operatorButtonValue = '';
+let result = '';
+
+const numpad = function() {
+    if(currentOperand.textContent != '0') {
+        currentOperand.textContent = ''
+    }
+    numButtons.forEach((numButton) => {
+        numButton.addEventListener('click', () => {
+            if(currentOperand.textContent === '0') {
+                currentOperand.textContent = '';
+            }
+            inputNum1 += numButton.textContent;
+            currentOperand.textContent += numButton.textContent ;
+            console.log('inputNum1 ' + inputNum1);
+        })
+    })
 }
 
 
-let inputNum = [];
-numButtons.forEach((numButton) => {
-    numButton.addEventListener('click', () => {
-        inputNum.push(numButton.textContent);
-        displayTxt.textContent = inputNum.join('');
-    })
-})
-
-
-let inputNum2 = [];
-let operatorButtonValue = '';
+//const newArray = numArray.reduce((firstOne, nextOne) => firstOne + nextOne, 0)
 
 //upon clicking any operator perform operation
 operatorButtons.forEach((operatorButton) => {
     operatorButton.addEventListener('click', () => {
-        inputNum2 = inputNum;
-        inputNum = [];
-        // console.log('inputNum2: '+parseInt(inputNum2.join('')));
-        // console.log('inputNum: '+inputNum.join(''));
-       operatorButtonValue = operatorButton.textContent;
-       console.log('Operator: '+operatorButtonValue);
-       })
+        if(inputNum2 ==='') {
+            inputNum2 = inputNum1;
+            inputNum1 = '';
+           operatorButtonValue = operatorButton.textContent;
+           previousOperand.textContent = inputNum2 + operatorButtonValue;
+           currentOperand.textContent = '0';
+        }
+        else {
+            result = operate(Number(inputNum2), operatorButtonValue, Number(inputNum1));
+            operatorButtonValue = operatorButton.textContent;
+            previousOperand.textContent = result + operatorButtonValue;
+            currentOperand.textContent = '0';
+            inputNum1 = '';
+            inputNum2 = result;
+        }
+    })
+})
+
+equalBtn.addEventListener('click', () => {
+    try {
+        result = operate(Number(inputNum2), operatorButtonValue, Number(inputNum1));
+        currentOperand.textContent = result;
+        inputNum1 = '';
+        inputNum2 = result;
+    }
+    catch(error){
+        currentOperand.textContent= 'ERROR'
+    }
+    previousOperand.textContent = '' 
+    inputNum1 = '';
+    inputNum2 = '';
 })
 
 decimalBtn.addEventListener('click', () => {
-    console.log(decimalBtn);
-    inputNum.push('.');
+    inputNum1 += decimalBtn.textContent;
+    currentOperand.textContent = inputNum1;
+
+
 }) 
 
-equalBtn.addEventListener('click', () => {
-    operate(parseInt(inputNum2.join('')),operatorButtonValue, parseInt(inputNum.join('')));
-    inputNum = [];
+posNegBtn.addEventListener('click', () => {
+    if(inputNum1 === 0) {
+        inputNum1 += +'-';
+        currentOperand.textContent = inputNum1;
+    }
 })
 
 allClear();
 deleteNum();
+numpad()
